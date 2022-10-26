759c82037688b4e2edec14d7d2d6fe2797f0f598
vadiksavintsev <113433935+vadiksavintsev@users.noreply.github.com>
2022-10-26 16:52:50 +0300

    Create gallery.js

---
+++ gallery.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+$('.responsive').slick({
+    dots: true,
+    arrows: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+        {
+            breakpoint: 1024,
+			settings: {
+			  slidesToShow: 4,
+			  slidesToScroll: 4,
+			  infinite: true,
+			  dots: true
+			}
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        }
+    ]
+});
+});
