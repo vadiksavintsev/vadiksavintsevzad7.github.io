c0e2497b1673c7cd3fd80a85b012cc16a5088d78
vadiksavintsev <113433935+vadiksavintsev@users.noreply.github.com>
2022-11-21 22:18:14 +0300

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,28 +1,27 @@
 $(document).ready(function(){
-$('.responsive').slick({
-    dots: true,
-    arrows: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    responsive: [
-        {
-            breakpoint: 1024,
-			settings: {
-			  slidesToShow: 4,
-			  slidesToScroll: 4,
-			  infinite: true,
-			  dots: true
-			}
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
+    $('.slider').slick({
+        arrows: true,
+        dots:true,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                  slidesToShow: 4,
+                  slidesToScroll: 4,
+                  infinite: true,
+                  dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
             }
-        }
-    ]
-});
-});
+        ]
+    });
+    });
