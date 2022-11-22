18fd8fe0cdda8568fd20cf9e721e8b6cc54ae33b
vadiksavintsev <113433935+vadiksavintsev@users.noreply.github.com>
2022-11-22 13:14:54 +0300

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -7,20 +7,10 @@ $(document).ready(function(){
         slidesToScroll: 4,
         responsive: [
             {
-                breakpoint: 1024,
-                settings: {
-                  slidesToShow: 4,
-                  slidesToScroll: 4,
-                  infinite: true,
-                  dots: true
-                }
-            },
-            {
                 breakpoint: 600,
                 settings: {
                     slidesToShow: 2,
                     slidesToScroll: 2
-                }
             }
         ]
     });
